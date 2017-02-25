fdf16f069d09a9611da277029342144f469a7f4c
CHARMAINE THAM <charmainetham@gmail.com>
2017-02-25 15:07:53 -0800

    added delete functionality

--- public/app.js
+++ public/app.js
@@ -5,10 +5,37 @@
 //event handler when post is created
   function BlogController($scope, $http){
     $scope.createPost = createPost;
+    $scope.deletePost = deletePost;
+
+    function init(){
+      getAllPosts();
+    };
+
+    init();
+
+
+    function getAllPosts(){
+      $http
+        .get("api/blogpost")
+        .success(function(posts){
+          $scope.posts = posts;
+        })
+    }
 
     function createPost(post) {
       console.log(post)
-      $http.post("/api/blogpost", post);
+      $http
+        .post("/api/blogpost", post)
+        .success(getAllPosts)
     }
+
+    function deletePost(postId){
+      console.log('Hey Imdeleting')
+      $http
+        .delete("/api/blogpost/"+postId)
+        .success(getAllPosts)
+    }
+
+
   }
 })();
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser')
 var app = express();
 var port = 3000
 var mongoose = require('mongoose');
-
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/codingbullblog');
 
 var PostSchema = mongoose.Schema({
@@ -20,14 +20,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.post("/api/blogpost", createPost)
+app.get("/api/blogpost", getAllPosts)
+app.delete("/api/blogpost/:id", deletePost)
+
+function getAllPosts(req,res){
+  PostModel
+    .find()
+    .then(
+      function(posts){
+        res.json(posts);
+      },
+      function(error){
+        res.sendStatus(400);
+      }
+      )
+}
 
 function createPost(req, res){
   var post = req.body;
   console.log(post)
-  PostModel.create(post);
+  PostModel
+    .create(post)
+    .then(
+      function(postObj){
+        res.json(200);
+      },  
+      function(error){
+        res.sendStatus(400);
+      }
+    );
   res.json(post)
 }
 
+function deletePost(req, res){
+  var postId = req.params.id;
+  PostModel
+    .remove({_id: postId})
+    .then(
+      function(status){
+        res.sendStatus(200)
+      },
+      function () {
+        res.sendStatus(400)
+      }
+      )
+}
 app.listen(port, function(){
   console.log('app listening on port ' + port)
 })
